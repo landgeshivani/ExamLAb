42e715677fc7e169698aca4a79c4164229f251d3
Shivani <landgeshivani@gmail.com>
2023-02-01 21:27:49 +0530

    yes done with this

--- Hello.java
+++ Hello.java
@@ -4,7 +4,10 @@ class Prithvi
 
     public static void main(String args[]) {
     
-    System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello shivani");
+        System.out.println("HelloHelloHello");
+        
     }
 
 }
