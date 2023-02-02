0651b4459e683a3aed20c4103324c18d4630bc13
Shivani <landgeshivani@gmail.com>
2023-02-02 15:22:37 +0530

    Fourth commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -19,30 +19,45 @@ class BubbleSort {
                 System.out.println();
 
             }
-            //display(a1);
+            
         }
     }
 
 
     static void InsertionSort(int a1[])
     {
-        int n=a1.length;
-        for(int i=1;i<n;++i)
-        {
+        int n = a1.length;
+        for (int i = 1; i < n; ++i) {
             int key = a1[i];
-            int j=i-1;
-            
-            while(j>=0 && a1[j] > key)
-            {
-                a1[j+1]=a1[j];
-                j=j-1;
+            int j = i - 1;
+
+            while (j >= 0 && a1[j] > key) {
+                a1[j + 1] = a1[j];
+                j = j - 1;
             }
-            a1[j+1]=key;
+            a1[j + 1] = key;
             display(a1);
-                    System.out.println();
+            System.out.println();
         }
     }
 
+    static void Selectionsort(int a1[])
+{
+	int n=a1.length;
+	for(int i=0;i<n-1;i++)
+	{
+		int min = i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(a1[j] < a1[min])
+				min =j;
+		}
+		//swapping 
+		int temp = a1[min];
+		a1[min]=a1[i];
+		a1[i]=temp;
+	}
+}
     static void display(int a1[])
 	{
 		int n =a1.length;
@@ -60,9 +75,7 @@ class BubbleSort {
 		System.out.println();
         bubblesort(a1);
         InsertionSort(a1);
-		
-		
-		
-		
+        Selectionsort(a1);
+
 	}
 }
